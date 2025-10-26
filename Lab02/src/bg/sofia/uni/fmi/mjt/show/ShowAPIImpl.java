bbcd5f3c10d49c6a4696e77b8a8b8f521643f950
Teodor Golisharski <t.golisharski@gmail.com>
2025-10-26 21:25:11 +0200

    ShowAPIImpl is implemented.

--- Lab02/src/bg/sofia/uni/fmi/mjt/show/ShowAPIImpl.java
+++ Lab02/src/bg/sofia/uni/fmi/mjt/show/ShowAPIImpl.java
@@ -4,10 +4,12 @@ import bg.sofia.uni.fmi.mjt.show.date.DateEvent;
 import bg.sofia.uni.fmi.mjt.show.elimination.EliminationRule;
 import bg.sofia.uni.fmi.mjt.show.ergenka.Ergenka;
 
-public class ShowAPIImpl implements ShowAPI{
+import java.util.Objects;
+
+public class ShowAPIImpl implements ShowAPI {
 
     private Ergenka[] ergenkas;
-    private EliminationRule[] defaultEliminationRules;
+    private final EliminationRule[] defaultEliminationRules;
 
     public ShowAPIImpl(Ergenka[] ergenkas, EliminationRule[] defaultEliminationRules) {
         this.ergenkas = ergenkas;
@@ -16,21 +18,33 @@ public class ShowAPIImpl implements ShowAPI{
 
     @Override
     public Ergenka[] getErgenkas() {
-        return ergenkas;
+        if (ergenkas.length > 0) {
+            return ergenkas;
+        }
+        return new Ergenka[0];
+
     }
 
     @Override
     public void playRound(DateEvent dateEvent) {
-
+        for (Ergenka e : ergenkas) {
+            e.reactToDate(dateEvent);
+        }
     }
 
     @Override
     public void eliminateErgenkas(EliminationRule[] eliminationRules) {
-
+        for (EliminationRule er : eliminationRules) {
+            ergenkas = er.eliminateErgenkas(ergenkas);
+        }
     }
 
     @Override
     public void organizeDate(Ergenka ergenka, DateEvent dateEvent) {
-
+        for (Ergenka e : ergenkas) {
+            if(Objects.equals(e.getName(), ergenka.getName())){
+                e.reactToDate(dateEvent);
+            }
+        }
     }
 }
