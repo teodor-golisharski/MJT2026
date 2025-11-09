d6e88eaa81869f657929d349acdaca668924dc00
Teodor Golisharski <t.golisharski@gmail.com>
2025-11-09 15:45:04 +0200

    Category fully implemented.

--- Lab03/src/bg/sofia/uni/fmi/mjt/burnout/subject/Category.java
+++ Lab03/src/bg/sofia/uni/fmi/mjt/burnout/subject/Category.java
@@ -1,8 +1,18 @@
 package bg.sofia.uni.fmi.mjt.burnout.subject;
 
 public enum Category {
-    MATH,
-    PROGRAMMING,
-    THEORY,
-    PRACTICAL
+    MATH(0.2),
+    PROGRAMMING(0.1),
+    THEORY(0.15),
+    PRACTICAL(0.05);
+
+    private final double restCoefficient;
+
+    Category(double restCoefficient) {
+        this.restCoefficient = restCoefficient;
+    }
+
+    public double getRestCoefficient() {
+        return restCoefficient;
+    }
 }
